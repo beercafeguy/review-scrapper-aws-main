5f63e44c23bbf9f81b0fb269ea05ad3b4ecf1659
beercafeguy <hemchandra@outlook.com>
2023-02-22 21:00:59 +0530

    Added mongo connection for reviews

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
+
+import sys, os
+sys.path.append(os.path.join(sys.path[0],'dao'))
+print(sys.path)
+
+import mongo_util
+
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
 
 app = Flask(__name__)
@@ -72,6 +79,7 @@ def index():
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
+            mongo_util.insert_reviews(reviews,"flipkart_reviews")
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             logging.info(e)
@@ -83,4 +91,4 @@ def index():
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",port="5001")
